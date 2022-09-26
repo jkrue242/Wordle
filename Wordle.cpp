ed89314e19c2fd494981998ebfbdffdfcbb8542f
jkrue242 <josephkrueger242@gmail.com>
2022-09-26 00:31:55 -0500

    Added Wordle Game

---
+++ Wordle.cpp
@@ -0,0 +1,182 @@
+// Written by Joseph Krueger
+
+#include "Wordle.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+//===============================================================
+Wordle::Wordle(std::string filename) {
+    setWordListFilename(filename);
+    srand(time(nullptr));
+    // initially setting availability for all letters to true
+    for(int i = 0; i < 26; i++)
+    {
+        availableLetters[i] = true;
+    }
+}
+
+//===============================================================
+void Wordle::loadWordList() {
+
+    // Make sure file was opened
+    string filename = getWordListFilename();
+    ifstream fin(filename);
+    if (fin.fail()){
+        cout << "Error: could not open "<<getWordListFilename()<<endl;
+    }
+    else
+    {
+        // Read how many words in file
+        string data;
+        int count = 0;
+        while (!fin.eof()){
+            getline(fin, data);
+            if(data == ""){break;}
+            count++;
+        }
+        fin.clear();
+
+        // Resize wordList based on how many words were found
+        wordList.resize(count);
+        fin.seekg(0, fin.beg);
+        count = 0;
+
+        // Populate wordList vector
+        while(!fin.eof()){
+            getline(fin, wordList[count]);
+            count++;
+        }
+
+        // Close file
+        fin.close();
+    }
+}
+
+//===============================================================
+void Wordle::printWordList() {
+    // Check if wordList is empty
+    if(wordList.size() == 0)
+    {
+        cout<<"Error: Word list empty."<<endl;
+    }
+
+    // Print each word in wordList if not empty
+    else
+    {
+        for(int i = 0; i < wordList.size(); i++)
+        {
+            cout<<wordList[i]<<endl;
+        }
+    }
+}
+
+//===============================================================
+void Wordle::setSecretWord(std::string word) {
+    // Check if word is in word list
+    if (find(wordList.begin(),wordList.end(),word) == wordList.end()){
+        std::cout<<"Error: Not a valid secret word."<<endl;
+    } else {
+        secretWord = word;
+    }
+}
+
+//===============================================================
+void Wordle::setRandomSecretWord() {
+    // get random number for index
+    int range = wordList.size();
+    int randomIndex = rand() % range;
+    setSecretWord(wordList[randomIndex]);
+}
+
+//===============================================================
+bool Wordle::makeGuess(std::string word) {
+    bool correct = false;
+    // filter out invalid guesses
+    if (find(wordList.begin(), wordList.end(), word) == wordList.end() && word.size() == 5) {
+        std::cout << "Error: " << word << " is not in word list. Try again." << endl;
+        return false;
+    } else if (word.size() != 5) {
+        std::cout << "Error: " << word << " is not 5 characters. Try again." << endl;
+        return false;
+    }
+        // if guess was valid
+    else {
+        guessNum++;
+        if (guessNum <= MAX_GUESS_NUM) {
+            // adding guess to list of guesses
+            guess.push_back(word);
+            if (word == secretWord) {
+                std::cout << "Congratulations!! You guessed the secret word."<<std::endl;
+                correct = true;
+            }
+
+            // determining feedback by comparing each letter in word and secretWord
+            std::string feed = "";
+            std::string secretCopy = secretWord;
+            for(int i = 0; i < 5; i++)
+            {
+                // if letter at index i matches
+                if(secretCopy[i] == word[i])
+                {
+                    feed.push_back('!');
+                    // making sure this letter does not get copied again
+                    secretCopy[i] = '-';
+                }
+                // if letter at index i does not match, but exists elsewhere in secretWord
+                else if(secretCopy.find(word[i]) != std::string::npos)
+                {
+                    feed.push_back('+');
+                }
+                // if letter at index i does not match and is not in secretWord anywhere
+                else
+                {
+                    // loop through ALPHABET array to set letter availability to false
+                    for(int j = 0; j < 26; j++)
+                    {
+                        // setting availability to false
+                        if(ALPHABET[j] == word[i])
+                        {
+                            // note: availableLetters and ALPHABET are same size array
+                            availableLetters[j] = false;
+                        }
+                    }
+                    feed.push_back('.');
+                }
+            }
+            // appending the created feedback to feedback vector
+            feedback.push_back(feed);
+        } else {
+            std::cout << "You did not guess the word. The word was: " << secretWord << ". Game Over." << std::endl;
+        }
+        return correct;
+    }
+}
+
+//===============================================================
+void Wordle::print() {
+    for(int i = 0; i < guessNum; i++)
+    {
+        cout<<i+1<<": "<<guess[i]<<" "<<feedback[i]<<endl;
+    }
+}
+
+//===============================================================
+void Wordle::printAvailableLetters() {
+    cout<<"Available letters to guess: ";
+    for(int i = 0; i < 26; i++)
+    {
+        if(availableLetters[i])
+        {
+            // no comma on last letter
+            if(i == 25)
+            {
+                cout<<ALPHABET[i]<<endl;
+            }
+            else
+            {
+                cout<<ALPHABET[i]<<", ";
+            }
+        }
+    }
+}
---
+++ Wordle.h
@@ -0,0 +1,48 @@
+// Written by Joseph Krueger
+#pragma once
+
+#include <string>
+#include <vector>
+//===============================================================
+
+class Wordle {
+public:
+    explicit Wordle(std::string filename="");
+
+    void setWordListFilename(std::string filename){this->wordListFilename = filename;}
+    std::string getWordListFilename(){return this->wordListFilename;}
+
+    void loadWordList();
+
+    void setSecretWord(std::string word);
+    std::string getSecretWord(){return this->secretWord;}
+    void setRandomSecretWord();
+
+    int getGuessNum(){return guessNum;}
+    bool makeGuess(std::string word);
+
+    void printWordList();
+    void print();
+    void printAvailableLetters();
+
+    // const alphabet array of english letters
+    const char ALPHABET[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                               'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                               'o', 'p', 'q', 'r', 's', 't', 'u',
+                               'v', 'w', 'x', 'y', 'z'};
+
+private:
+    std::string wordListFilename;
+    std::vector<std::string> wordList;
+
+    std::string secretWord;
+
+    int guessNum = 0;
+
+    // const max number of guesses to be used in the game
+    const int MAX_GUESS_NUM = 6;
+    std::vector<std::string> guess;
+    std::vector<std::string> feedback;
+
+    bool availableLetters[26];
+};
