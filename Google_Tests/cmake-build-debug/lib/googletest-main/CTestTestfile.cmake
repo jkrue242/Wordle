3c4621a9d188dffcb9d0df6a7b14eeb2601061a3
jkrue242 <josephkrueger242@gmail.com>
2022-09-26 16:12:02 -0500

    added Google Tests

---
+++ Google_Tests/cmake-build-debug/lib/googletest-main/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/josephkrueger/projects/Wordle/Google_Tests/lib/googletest-main
+# Build directory: /Users/josephkrueger/projects/Wordle/Google_Tests/cmake-build-debug/lib/googletest-main
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ Google_Tests/cmake-build-debug/lib/googletest-main/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/josephkrueger/projects/Wordle/Google_Tests/lib/googletest-main/googletest
+# Build directory: /Users/josephkrueger/projects/Wordle/Google_Tests/cmake-build-debug/lib/googletest-main/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
